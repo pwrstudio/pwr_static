33f2f58e669a92af1814fa43d83dd9526a639093
Rasmus Svensson <rasmus@Rasmuss-Air.home>
2016-02-14 15:14:41 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,123 @@
+// Include gulp
+const gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  autoprefixer = require('gulp-autoprefixer'),
+  rename = require('gulp-rename'),
+  plumber = require('gulp-plumber'),
+  cache = require('gulp-cache'),
+  cssnano = require('gulp-cssnano'),
+  handlebars = require('gulp-handlebars'),
+  sass = require('gulp-sass'),
+  wrap = require('gulp-wrap'),
+  declare = require('gulp-declare'),
+  notify = require('gulp-notify'),
+  imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant'),
+  changed = require('gulp-changed'),
+  parker = require('gulp-parker'),
+  fontgen = require('gulp-fontgen'),
+  browserSync = require('browser-sync').create();
+
+// Concatenate & Minify JS
+gulp.task('scripts', function () {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+});
+
+// Handle styles
+gulp.task('sass', function () {
+  return gulp.src('src/style/style.scss')
+    .pipe(changed('./'))
+    .pipe(plumber({
+      errorHandler: sassErrorAlert
+    }))
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+});
+
+// Browser Sync
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    proxy: 'localhost:8888',
+    open: false
+  });
+});
+
+// Images
+gulp.task('images', () => {
+  return gulp.src('src/img/*')
+    .pipe(changed('img'))
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+        }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('img'));
+});
+
+gulp.task('font', function () {
+  return gulp.src("./src/fonts/*.{ttf,otf}")
+    .pipe(fontgen({
+      dest: "./fonts/"
+    }));
+});
+
+// Watch for changes in files
+gulp.task('watch', function () {
+
+  // Watch .js files
+  gulp.watch('src/js/*.js', ['scripts']);
+
+  // Watch .html files
+  gulp.watch('*.html', browserSync.reload);
+
+  // Watch .scss files
+  gulp.watch('src/style/*.scss', ['sass']);
+
+  // Watch images
+  gulp.watch('src/img/*', ['images']);
+
+  // Watch fonts
+  gulp.watch('src/fonts/*.{ttf,otf}"', ['font']);
+});
+
+// Analyze CSS
+gulp.task('parker', function () {
+  return gulp.src('./style.css')
+    .pipe(parker());
+});
+
+// Default Task
+gulp.task('default', ['scripts', 'sass', 'watch', 'browser-sync', 'images', 'font']);
+
+function sassErrorAlert(error) {
+  notify.onError({
+    title: 'SCSS Error',
+    message: error.message,
+    sound: 'Submarine'
+  })(error); //Error Notification
+  console.log(error.toString()); //Prints Error to Console
+  this.emit('end'); //End function
+};
+
+function handlebarsErrorAlert(error) {
+  notify.onError({
+    title: 'Handlebars Error',
+    message: error.message,
+    sound: 'Ping'
+  })(error); //Error Notification
+  console.log(error.toString()); //Prints Error to Console
+  this.emit('end'); //End function
+};
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/*jslint browser: true, devel: true, node: true, nomen: true, plusplus: true*/
+/*global $, jQuery*/
+
+(function () {
+
+  "use strict";
+
+  $(function () {
+    console.log("ready!");
+  });
+
+}());
