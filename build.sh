4f5e6672247d904a41a186b65505e961e84384dd
PWR <cell@pwr.site>
2016-06-02 10:42:01 +0200

    added gitkeep to fonts/img directories. fixed build-script to include html files. added standard js.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ TIME=`date +%Y-%m-%d-%H-%M`
 NAME=${PWD##*/}
 NICK=`gshuf -n 1 /usr/share/dict/words`
 mkdir -v ../$NAME-$TIME-$NICK
-cp -v ./*.php ../$NAME-$TIME-$NICK/
+cp -v ./*.html ../$NAME-$TIME-$NICK/
 cp -v ./app.min.js ../$NAME-$TIME-$NICK/
 cp -v ./style.css ../$NAME-$TIME-$NICK/
 rsync -av ./img/ ../$NAME-$TIME-$NICK/img
