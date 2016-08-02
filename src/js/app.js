ff290ab8a63cfb94499b21f31d0ab5443a5b0f58
PWR <cell@pwr.site>
2016-08-02 21:22:36 +0200

    connection to socket api server

--- src/js/app.js
+++ src/js/app.js
@@ -4,35 +4,64 @@
 (function () {
 
   "use strict";
-  
-  // Require jQuery
-  global.$                = require("jquery");
-    
-  var shared              = require("./shared"),
-      debounce            = require('debounce');
-      
-  //  require("swiper");
-  //  require("fullpage.js");
-  
-  // Modernizr tests
-  // require('browsernizr/test/webgl');
-  // require('browsernizr/test/workers/webworkers');
-  // require('browsernizr/test/webrtc/peerconnection');
-  // require('browsernizr/test/storage/localstorage');
-  // require('browsernizr/test/audio/webaudio');
-  // require('browsernizr/test/websockets');
-  // var Modernizr = require('browsernizr');
-  
-  $(function () {
-    
-    console.log("READY");
-    
-    //    var swiper = new Swiper('.swiper-container', {
-    //      speed: 1000
-    //      autoplay: 40
-    //    });
 
-    //    $('#fullpage').fullpage();  
+  // Require jQuery
+  global.$ = require("jquery");
+
+  require('browsernizr/test/audio/webaudio');
+  require('browsernizr/test/websockets');
+  var Modernizr = require('browsernizr');
+
+  function updateViews(data) {
+
+    $('#screen-one')
+      .find('img')
+      .attr('src', data.state[0].URL);
+
+    $('#screen-two')
+      .find('img')
+      .attr('src', data.state[1].URL);
+
+    $('#screen-three')
+      .find('img')
+      .attr('src', data.state[2].URL);
+
+    $('#screen-four')
+      .find('img')
+      .attr('src', data.state[3].URL);
+
+  }
+
+  $(function () {
+
+    console.log("READY");
+
+    // var socketServer = 'http://192.168.0.65:5000';
+    var socketServer = 'https://glacial-reaches-83593.herokuapp.com';
+
+    var socket = io(socketServer, {
+      'transports': ['websocket', 'polling']
+    });
+
+    console.log(socket);
+
+    socket.on('connect', function () {
+      console.log('Socket connected');
+    });
+
+    socket.on('init', function (data) {
+      console.log('&&&&&&INIT');
+      updateViews(data);
+    });
+
+    socket.on('cut', function (data) {
+      console.log(data.state);
+      updateViews(data);
+    });
+
+    socket.on('disconnect', function () {
+      console.log('Socket disconnected');
+    });
 
   });
 
